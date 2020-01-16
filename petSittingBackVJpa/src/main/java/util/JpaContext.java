9076bc8be0ad393403c346764472e4c3862515fd
Gael <gael.beuzit@gmail.com>
2020-01-16 11:40:06 +0100

    ajout dossiers front et back

---
+++ petSittingBackVJpa/src/main/java/util/JpaContext.java
@@ -0,0 +1,25 @@
+package util;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JpaContext { //On construit la factory pour l'emf. pour qu'il n'y en ai qu'un à tout instant. l'emf est en static pour etre dispo partout dans le projet.
+
+		private static EntityManagerFactory emf = null;
+		
+		public static EntityManagerFactory getInstance() {
+			if(emf==null) {
+			emf = Persistence.createEntityManagerFactory("jpa");
+			}
+				return emf;
+			
+		}
+		
+		public static void destroy() {
+			if(emf!=null) {
+				emf.close();
+				emf = null;
+			}
+		}
+		
+}
