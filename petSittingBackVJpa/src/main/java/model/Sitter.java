eb9c08716f57fd79c3e62c0489826c9571030d9d
Gael <gael.beuzit@gmail.com>
2020-01-17 16:54:25 +0100

    vgs

--- petSittingBackVJpa/src/main/java/model/Sitter.java
+++ petSittingBackVJpa/src/main/java/model/Sitter.java
@@ -1,11 +1,18 @@
 package model;
 
+import java.util.List;
 import java.util.Set;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
-
+import javax.persistence.EntityManager;
 import javax.persistence.OneToMany;
+
+import dao.DaoAnnonce;
+import dao.DaoAnnonceFactory;
+import dao.DaoReponse;
+import dao.DaoReponseFactory;
+import util.JpaContext;
 
 
 
@@ -23,7 +30,30 @@ public class Sitter extends Compte {
 
 	}
 
-	
+//	public static List<Annonce> afficherAnnoncesEnCours(){ // attention : supprimer static dans classe ?.
+//		EntityManager em=JpaContext.getInstance().createEntityManager();
+//		DaoAnnonce daoAnnonce = DaoAnnonceFactory.getInstance(); // attention : supprimer ligne dans dao.
+//		List<Annonce> annonces=null;
+////		annonces = SelectAllWithStatut0(); //Attention : a remplacer par ligne du dessous.
+//		annonces = daoAnnonce.SelectAllWithStatut0();
+//		
+//		em.close();
+//
+//		return annonces;
+//	}
+//	
+//	public static void postulerAnnonce(Sitter s, Annonce a, String message) {
+//		EntityManager em=JpaContext.getInstance().createEntityManager();
+//		DaoReponse daoReponse = DaoReponseFactory.getInstance(); // attention : supprimer ligne dans dao.
+//		ReponsePK key = new ReponsePK((Sitter) s, a);
+//		Reponse rep = new Reponse(key, message);
+//		System.out.println(s.getNumC());
+//		daoReponse.insert(rep);
+//		
+//		
+//		em.close();
+//		
+//	}
 	
 
 	
