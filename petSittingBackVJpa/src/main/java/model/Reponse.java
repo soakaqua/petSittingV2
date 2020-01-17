4dced56a60099b84108ef579f2ac35c1b972d645
Gael <gael.beuzit@gmail.com>
2020-01-17 10:01:23 +0100

    v gael

--- petSittingBackVJpa/src/main/java/model/Reponse.java
+++ petSittingBackVJpa/src/main/java/model/Reponse.java
@@ -1,9 +1,18 @@
 package model;
 
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "reponse")
 public class Reponse {
 	
-	
-	
+	@EmbeddedId
+	private ReponsePK key;
+	@Column(name = "reponse", length =200 )
+	private String reponse;
 	
 	
 	
